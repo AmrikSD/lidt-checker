1392696b5d17d89a29482337f91a5c096b61e113
AmrikSD <amrik.singh@live.co.uk>
2022-09-11 01:40:08 +0100

    Port everything to work for LIDT

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,32 +18,62 @@ type Author struct {
 	IconURL string `json:"icon_url"`
 }
 
+type Field struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline"`
+}
+
+type Image struct {
+	URL string `json:"url"`
+}
+
+type Footer struct {
+	Text    string `json:"text"`
+	IconURL string `json:"icon_url"`
+}
+
 type Embed struct {
-	Title  string `json:"title"`
-	URL    string `json:"url"`
-	Colour int    `json:"color"`
-	Author Author `json:"author"`
+	Author      Author  `json:"author"`
+	Title       string  `json:"title"`
+	URL         string  `json:"url"`
+	Description string  `json:"description"`
+	Colour      int     `json:"color"`
+	Fields      []Field `json:"fields,omitempty"`
+	Thumbnail   Image   `json:"thumbnail,omitempty"`
+	Image       Image   `json:"image,omitempty"`
+	Footer      Footer  `json:"footer,omitempty"`
 }
 
 type DiscordMessage struct {
-	Content   string  `json:"content,omitempty"`
-	Embeds    []Embed `json:"embeds"`
 	Username  string  `json:"username"`
 	AvatarURL string  `json:"avatar_url"`
+	Content   string  `json:"content,omitempty"`
+	Embeds    []Embed `json:"embeds"`
 }
 
 func handler() {
-	status := getJapanStatus()
-	updateDiscord(status)
+	availableTests := getLidtTests("Tolworth (London)")
+	if len(availableTests) > 0 {
+		updateDiscord(availableTests)
+	}
 }
 
 func main() {
-	lambda.Start(handler)
+	environment := os.Getenv("APP_ENV")
+	if environment == "DEV" {
+		handler()
+	} else if environment == "PROD" {
+		lambda.Start(handler)
+	} else {
+		log.Fatal("Environment not correct")
+	}
 }
 
-func getJapanStatus() string {
+func getLidtTests(centre string) []Field {
 
-	response, err := http.Get("https://www.japan-guide.com/news/alerts.html")
+	response, err := http.Get("https://lidt.co.uk/fast-track-booking")
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,69 +81,66 @@ func getJapanStatus() string {
 
 	document, err := goquery.NewDocumentFromReader(response.Body)
 	if err != nil {
-		log.Fatal("Error loading HTTP reponse body. ", err)
+		log.Fatal("Error loading HTTP reponse body.", err)
 	}
 
-	status := document.Find("div.post_list__content:nth-child(3) > p:nth-child(2)").First().Text()
-	lastUpdated := document.Find(".post_list__date").First().Text()
-	return fmt.Sprintf("%s\n%s", status, lastUpdated)
+	var testInWantedLocation []Field
+
+	testOptions := document.Find("#testCentres > option")
+	testOptions.Each(func(_ int, testOption *goquery.Selection) {
+		location, exists := testOption.Attr("data-location")
+		time := testOption.AttrOr("data-start", "unknown")
+
+		if exists && location == centre {
+			testInWantedLocation = append(testInWantedLocation, Field{
+				Name:  location,
+				Value: time,
+			})
+		} else if strings.Contains(testOption.Text(), "Tolworth") {
+			testInWantedLocation = append(testInWantedLocation, Field{
+				Name:  "Tolworth (maybe)",
+				Value: "Unknown",
+			})
+		}
+	})
+
+	return testInWantedLocation
 }
 
 func checkEnvExists(str string) {
 	_, exists := os.LookupEnv(str)
 	if !exists {
-		log.Fatal("Environment variable not set: ", str)
+		log.Fatalf("Environment variable not set: %s", str)
 		os.Exit(1)
 	}
 }
 
-func updateDiscord(status string) {
+func updateDiscord(fields []Field) {
 
+	checkEnvExists("WEBHOOK_URL")
 	webhookURL, _ := os.LookupEnv("WEBHOOK_URL")
-	messageLink, _ := os.LookupEnv("MESSAGE_LINK")
-	authorName, _ := os.LookupEnv("AUTHOR_NAME")
-	authorURL, _ := os.LookupEnv("AUTHOR_URL")
-	authorIcon, _ := os.LookupEnv("AUTHOR_ICON")
-	messageColourStr := os.Getenv("MESSAGE_COLOUR")
-	username, _ := os.LookupEnv("WEBHOOK_USERNAME")
-	avatarURL, _ := os.LookupEnv("AVATAR_URL")
-
-	messageColour, err := strconv.Atoi(messageColourStr)
-	if err != nil {
-		panic(err)
-	}
-
-	envVars := [8]string{
-		"WEBHOOK_URL",
-		"MESSAGE_LINK",
-		"AUTHOR_NAME",
-		"AUTHOR_URL",
-		"AUTHOR_ICON",
-		"MESSAGE_COLOUR",
-		"WEBHOOK_USERNAME",
-		"AVATAR_URL",
-	}
-
-	for _, str := range envVars {
-		checkEnvExists(str)
-	}
 
 	msg := DiscordMessage{
+		Username:  "Asbo - Webhook",
+		AvatarURL: "https://cdn.discordapp.com/avatars/122650490289913856/0da6bbf0c4c1c7006b578dd189a4c244",
+		Content:   "<@&681217602432794627>",
 		Embeds: []Embed{
 			{
-				Title:  status,
-				URL:    messageLink,
-				Colour: messageColour,
 				Author: Author{
-					Name:    authorName,
-					URL:     authorURL,
-					IconURL: authorIcon,
+					Name:    "AmrikSD",
+					URL:     "https://github.com/lidt-checker",
+					IconURL: "https://avatars.githubusercontent.com/u/17920436",
+				},
+				Title:  "Wanted Tests Available!",
+				URL:    "https://lidt.co.uk/fast-track-booking",
+				Colour: 12345678,
+				Fields: fields, //Passed into the method
+				Footer: Footer{
+					Text:    "https://github.com/AmrikSD/lidt-checker",
+					IconURL: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
 				},
 			},
 		},
-
-		Username:  username,
-		AvatarURL: avatarURL,
 	}
 
 	bytesRepresentation, err := json.Marshal(msg)
@@ -126,5 +152,4 @@ func updateDiscord(status string) {
 
 	var result map[string]interface{}
 	json.NewDecoder(resp.Body).Decode(&result)
-
 }
